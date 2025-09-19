2f3419dd6accae1a5fa3c792daecc2793b3ab13a
Rishabh Sehgal <rishabbh004@gmail.com>
2025-09-19 15:46:25 +0530

    fix: fixed references

--- bin/utils.js
+++ bin/utils.js
@@ -1,5 +1,10 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Recreate __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function copyTemplate(fileName, targetDir) {
   const srcPath = path.resolve(__dirname, "../runtime", fileName);
